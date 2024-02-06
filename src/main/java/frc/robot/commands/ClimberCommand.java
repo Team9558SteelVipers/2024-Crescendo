6cd759b3938fcd8e526982efdd1337ef31e4d2c6
elinaelizondo <148294044+elinaelizondo@users.noreply.github.com>
2024-02-05 19:27:08 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/commands/ClimberCommand.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+import frc.robot.subsystems.*;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.commands.*;
+
+import frc.robot.OI;
+
+
+import frc.robot.subsystems.ClimberSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.Timer;
+
+
+/** An example command that uses an example subsystem. */
+public class ClimberCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  
+  private final ClimberSubsystem m_subsystem;
+
+  public boolean isAuto = true;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   * @param i
+   */
+  public ClimberCommand( ClimberSubsystem subsystem) {
+    m_subsystem = subsystem;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (OI.getController().x().getAsBoolean()) {
+      m_subsystem.climberMotorSpeed(0.5);
+    } else if (OI.getController().y().getAsBoolean()){
+      m_subsystem.climberMotorSpeed(-0.5);
+    } else {
+      //Do Nothing
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+
+}
