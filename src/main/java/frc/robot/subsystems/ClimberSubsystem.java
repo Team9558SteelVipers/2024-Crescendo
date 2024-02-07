aaebaaaeee530c9d195075965ad3ede72a1264ad
moonlight689 <madiharahman689@gmail.com>
2024-02-07 17:56:47 -0500

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,12 +3,31 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
-  /** Creates a new ClimberSubsystem. */
-  public ClimberSubsystem() {}
+  private static final int climberMotorPort = 0;
+  private final TalonFX climberMotor; 
+
+  public ClimberSubsystem() {
+    climberMotor = new TalonFX(climberMotorPort);
+  }
+
+  public void climberUp (double speed){
+    climberMotor.set(speed);
+  }
+
+  public void climberDown(double speed){
+    climberMotor.set(-speed);
+  }
+
+  public void climberStop (double speed){
+    //digital input
+    if (topLimitSwitch.get()){
+      climberMotor.set(speed = 0);
+    }
+  }
 
   @Override
   public void periodic() {
