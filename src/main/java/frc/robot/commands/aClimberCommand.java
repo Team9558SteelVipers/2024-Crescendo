5213d7574a0a8cfd5581ed24f5127e47345ac2eb
Shabihad <148512968+Shabihad@users.noreply.github.com>
2024-02-07 18:02:36 -0800

    Adding Climber Command

--- src/main/java/frc/robot/commands/aClimberCommand.java
+++ src/main/java/frc/robot/commands/aClimberCommand.java
@@ -1,79 +1,53 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
-
 
 import frc.robot.OI;
 import frc.robot.subsystems.ClimberSubsystem;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
 
 /** An example command that uses an example subsystem. */
 public class ClimberCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  
+  //@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final ClimberSubsystem m_subsystem;
-  public boolean isAuto = true;
+  private OI m_controller;
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    * @param i
    */
-  public ClimberCommand(ClimberSubsystem subsystem, int time) {
+  public ClimberCommand( ClimberSubsystem subsystem, OI driverController) {
     m_subsystem = subsystem;
+    m_controller = driverController;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
-
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     
   }
-        public class ClimbUp {
-          private final Climber climber;
-          public ClimbUp(Climber subsystem) {
-            this.climber = subsystem;
-            addRequirements(climber);
-        }
-        public class ClimbDown extends CommandBase{
-         private final Climber climber;
- 
-        }
-          
-          public ClimbDown(Climber subsystem) {
-            this.climber = subsystem;
-            addRequirements(climber);
-
-        }
-
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (OI.getController().x().getAsBoolean()) {
+    // Button Inputs
+    if (m_controller.getDXButton()) {
       m_subsystem.climberMotorSpeed(0.5);
-    } else if (OI.getController().y().getAsBoolean()){
+    } else if (m_controller.getDYButton()){
       m_subsystem.climberMotorSpeed(-0.5);
     } else {
       //Do Nothing
     }
   }
-
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished() {;
-    if (finished = true) {
-      isAuto = false;
-    }
-    return finished;
-  }
+  public boolean isFinished() {
+return false;
+}
 }
