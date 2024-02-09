ed14cd026a29b9b2e358e7e4d3b732699f75e17b
Moltyn3000 <mathyoojr@gmail.com>
2024-02-09 18:24:15 -0500

    a

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,8 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+   
     // Configure the trigger bindings
     configureBindings();
   }
