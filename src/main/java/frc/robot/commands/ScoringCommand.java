fb08c75ad3f4f37e603bd702bbc7b9768675a040
Joshua Tran <jtran1246@gmail.com>
2024-03-23 12:39:16 -0400

    scoring command

---
+++ src/main/java/frc/robot/commands/ScoringCommand.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ClawScoringSubsystem;
+
+
+public class ScoringCommand extends Command {
+  
+ 
+  private static ClawScoringSubsystem m_clawScoringSubsystem;
+  
+  
+
+  public ScoringCommand(ClawScoringSubsystem clawScoringSubsystem){
+    
+    m_clawScoringSubsystem = clawScoringSubsystem;
+    addRequirements(clawScoringSubsystem);
+  }
+
+  
+  @Override
+  public void initialize() {
+
+    
+  }
+
+ 
+  @Override
+  public void execute() {
+
+    
+    m_clawScoringSubsystem.setIntakeMotorSpeed(-0.75);
+
+  }
+
+  
+  @Override
+  public void end(boolean interrupted) {
+    m_clawScoringSubsystem.setIntakeMotorSpeed(0.0);
+  }
+
+  
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
